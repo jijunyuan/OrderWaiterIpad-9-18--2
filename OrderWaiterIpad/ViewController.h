c6269f5a309354e81ac12b69f2338cd738ad441a
Noproblemgo <562578343@qq.com>
2013-09-11 19:15:58 +0800

    Initial Commit

---
+++ OrderWaiterIpad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OrderWaiterIpad
+//
+//  Created by tiankong360 on 13-9-11.
+//  Copyright (c) 2013年 tiankong360. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OrderWaiterIpad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OrderWaiterIpad
+//
+//  Created by tiankong360 on 13-9-11.
+//  Copyright (c) 2013年 tiankong360. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
